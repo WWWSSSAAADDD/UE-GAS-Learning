fe973b78ddaa1a53b84a39ddafb8e7d3d783014f
WWWSSSAAADDD <3781375775@qq.com>
2026-04-07 12:03:16 +0800

    实现Globe Widget

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,9 @@
+// Copyright 
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController) {
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Public/UI/Widget/AuraUserWidget.h
@@ -0,0 +1,26 @@
+// Copyright 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AuraUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable)
+	void SetWidgetController(UObject* InWidgetController);
+	
+	UFUNCTION(BlueprintImplementableEvent)
+	void WidgetControllerSet();
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UObject> WidgetController;
+};
