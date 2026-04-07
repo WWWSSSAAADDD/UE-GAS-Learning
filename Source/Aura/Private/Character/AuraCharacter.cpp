bc10b17beb58cd086420fd8e526935706df52bb4
WWWSSSAAADDD <3781375775@qq.com>
2026-04-07 22:27:13 +0800

    修正上一次提交GetPlayerState缺少的圆括号

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -47,7 +47,7 @@ void AAuraCharacter::InitAuraHUD()
 	// 多人联机时，非本机玩家的GetController返回nullptr，这种情况我们不想让程序崩溃
 	if (APlayerController* PC = Cast<APlayerController>(GetController()))
 	{
-		Cast<AAuraHUD>(PC->GetHUD())->InitOverlay(PC, GetPlayerState, AbilitySystemComponent, AttributeSet);
+		Cast<AAuraHUD>(PC->GetHUD())->InitOverlay(PC, GetPlayerState(), AbilitySystemComponent, AttributeSet);
 	}
 
 }
