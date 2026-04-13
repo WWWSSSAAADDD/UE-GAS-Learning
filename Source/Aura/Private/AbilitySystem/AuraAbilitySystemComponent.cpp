c1d394d93b9471c5468f110531cea6a4280b9f26
WWWSSSAAADDD <3781375775@qq.com>
2026-04-13 22:34:40 +0800

    学习使用GEngine打印信息，简单应用EffectApplied在ASC给自己添加GE时打印Tag信息

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -11,4 +11,12 @@ void UAuraAbilitySystemComponent::AbilityActorInfoSet()
 void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& GameplayEffectSpec, FActiveGameplayEffectHandle ActiveGameplayEffectHandle)
 {
 	GEngine->AddOnScreenDebugMessage(1, 2.f, FColor::Blue, FString("Gameplay Effect Applied"));
+
+	FGameplayTagContainer TagContainer;
+	GameplayEffectSpec.GetAllAssetTags(TagContainer);
+	for (const FGameplayTag& Tag : TagContainer)
+	{
+		FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+		GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Black, Msg);
+	}
 }
